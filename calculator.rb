c75a82693a4acefbca25db7bf37bbe1c98cc4905
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2020-12-26 02:47:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,5 @@ first_number =
 second_number = 
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
