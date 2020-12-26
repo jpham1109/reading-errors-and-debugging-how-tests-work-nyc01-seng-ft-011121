72db5d471afd78e638b8868edfc335df6b74fc51
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2020-12-26 02:49:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 first_number = get_variables_from_file
-second_number = 
+second_number = get_variables_from_file
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
